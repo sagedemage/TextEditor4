9a64ab2b0475c8885a6359c91384c38bbdc203d4
Salmaan Saeed <salsaeed999@gmail.com>
2024-03-14 23:34:11 -0400

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.texteditor4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.texteditor4 to javafx.fxml;
+    exports org.example.texteditor4;
+}
